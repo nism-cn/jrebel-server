174b9ffc5ac3aaa8a314d4fb39a57da8cee66665
inism <nism@aliyun.com>
2023-02-27 16:45:41 +0800

    add open browse with url

---
+++ src/main/java/org/nism/jrebel/utils/BrowseUtil.java
@@ -0,0 +1,41 @@
+package org.nism.jrebel.utils;
+
+import java.lang.reflect.Method;
+
+/**
+ * browse util
+ *
+ * @author inism
+ */
+public class BrowseUtil {
+
+    public static final String[] BROWSERS = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
+
+    private BrowseUtil() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void open(String url) throws Exception {
+        String osName = System.getProperty("os.name", "");
+        if (osName.startsWith("Windows")) {
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } else if (osName.startsWith("Mac OS")) {
+            Class<?> fileMgr = Class.forName("com.apple.eio.FileManager");
+            Method openURL = fileMgr.getDeclaredMethod("openURL", String.class);
+            openURL.invoke(null, url);
+        } else {
+            String browser = null;
+            for (String b : BROWSERS) {
+                if (Runtime.getRuntime().exec(new String[]{"which", b}).waitFor() == 0) {
+                    browser = b;
+                    break;
+                }
+            }
+            if (browser == null) {
+                throw new Exception("Could not find web browser");
+            } else {
+                Runtime.getRuntime().exec(new String[]{browser, url});
+            }
+        }
+    }
+}
