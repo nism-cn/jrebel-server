fddc4427a3738c01f9d4ca6468e693918e9f944d
inism <nism@aliyun.com>
2023-07-03 16:54:11 +0800

    fix browse bug

--- src/main/java/org/nism/jrebel/App.java
+++ src/main/java/org/nism/jrebel/App.java
@@ -14,17 +14,16 @@ import java.util.Map;
 public class App {
 
     public static void main(String[] args) {
-        Solon.start(App.class, args, app -> {
-            Map<String, String> arguments = MainUtil.parseArguments(args);
-            String port = arguments.get("p");
-            port = port == null || !port.matches("\\d+") ? "8080" : port;
-            app.cfg().setProperty("server.port", port);
-            try {
-                BrowseUtil.open("http://localhost:" + port);
-            } catch (Exception e) {
-                System.err.println("can not auto open browse!");
-            }
-        });
+        Map<String, String> arguments = MainUtil.parseArguments(args);
+        String port = arguments.get("p");
+        port = port == null || !port.matches("\\d+") ? "8080" : port;
+        try {
+            String finalPort = port;
+            Solon.start(App.class, args, app -> app.cfg().setProperty("server.port", finalPort));
+            BrowseUtil.open("http://localhost:" + port);
+        } catch (Exception e) {
+            System.err.println("can not auto open browse!");
+        }
     }
 
 }
